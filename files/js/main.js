82dab6f7d29a7cb7da36632118df8784cf3a0f4f
sbowennn <sbowennn@hotmail.com>
2025-01-24 10:44:41 +0000

    changed menu, products grid and few css rules

--- files/js/main.js
+++ files/js/main.js
@@ -17,4 +17,13 @@
 
     };
 
-// 
+// MOBILE MENU
+
+function mobileMenu() {
+  var x = document.getElementById("menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
